eae394229f40212d89153a373cbaab276c07e396
Roger Rodrigues <alewsor@gmail.com>
2026-06-25 15:01:59 +0530

    add applied date section

--- main.js
+++ main.js
@@ -5,11 +5,20 @@ const { searchPermits } = require("./advanced.search.js");
 const { fetchPermits } = require("./index.js");
 const { processEntireFolder } = require("./daily.js");
 
+// const FROM = "2020-05-31T18:30:00.000Z";
+// const TO = "2026-06-24T18:30:00.000Z";
+const FROM = "";
+const TO = "";
+
 const payload = JSON.parse(fs.readFileSync("search.payload.json", "utf-8"));
-payload.advanced.case.permitType = "CARA";
-payload.advanced.case.issuedDateFrom = "2025-12-31T18:30:00.000Z";
-payload.case.permitType = "CARA";
-payload.case.issuedDateFrom = "2025-12-31T18:30:00.000Z";
+payload.advanced.case.permitType = "CACA";
+// payload.advanced.case.issuedDateFrom = "2025-12-31T18:30:00.000Z";
+payload.advanced.case.enteredDateFrom = FROM;
+payload.case.enteredDateFrom = FROM;
+payload.advanced.case.enteredDateTo = TO;
+payload.case.enteredDateTo = TO;
+payload.case.permitType = "CACA";
+// payload.case.issuedDateFrom = "2025-12-31T18:30:00.000Z";
 
 async function main() {
   const cloudflare = await getCloudflareCookies(
